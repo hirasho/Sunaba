558cf6f3d07c9c88dfd3089f49a24d9ed70b0a1b
Hirayama Takashi <hirayama-takashi@kayac.com>
2022-05-20 18:38:31 +0900

    仮でファイル名入力をつけた

--- SunabaUnity/Assets/Main.cs
+++ SunabaUnity/Assets/Main.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 namespace Sunaba
 {
@@ -10,9 +11,11 @@ namespace Sunaba
 		[SerializeField] RawImage appWindow;
 		[SerializeField] Text messageWindow;
 		[SerializeField] Button rebootButton;
+		[SerializeField] InputField filenameInput;
 
 		void Start()
 		{
+			rebootButton.onClick.AddListener(OnClickReboot);
 			pixels = new Texture2D(100, 100);
 			messageStream = new System.IO.MemoryStream();
 			messageStreamWriter = new System.IO.StreamWriter(messageStream);
@@ -26,15 +29,20 @@ namespace Sunaba
 			
 		}
 
-		void OnProgramDrop()
-		{
-//			machine = new Machine();
-		}
-
 		// non public --------
 		Texture2D pixels;
 		Machine machine;
 		System.IO.StreamWriter messageStreamWriter;
 		System.IO.MemoryStream messageStream;
+
+		void OnClickReboot()
+		{
+			// ファイル名取って
+			// 開けて読んで
+			// コンパイラに渡して
+			// アセンブラに渡して
+			// Machineを起動
+//			machine = new Machine();			
+		}
 	}
 }
